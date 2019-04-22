04be98e0ff839a4c54370764e57f27b839903017
ievgenii nikitchenko <nakyola@gmail.com>
2019-04-23 00:18:52 +0300

    Added FavoriteList

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const cardComponent = {
         <hr v-show="card.isShowSubtitle" />
         <span v-if="card.subtitle && card.isShowSubtitle">{{ card.subtitle }}</span>
 
-        <div class="removeIcon" v-on:click="requestRemoveCard(index)" title="Remove this Card">+</div>
-        <div class="hideSubtitle" title="Hide Subtitle" v-if="card.isShowSubtitle" v-on:click="requestHideSubtitle(card)">&uArr;</div>
-        <div class="showSubtitle" title="Show Subtitle" v-if="card.subtitle && !card.isShowSubtitle" v-on:click="requestShowSubtitle(card)">&dArr;</div>
+        <div class="removeIcon" @click="requestRemoveCard" title="Remove this Card">+</div>
+        <div class="hideSubtitle" title="Hide Subtitle" v-if="card.isShowSubtitle" @click="requestHideSubtitle(card)">&uArr;</div>
+        <div class="showSubtitle" title="Show Subtitle" v-if="card.subtitle && !card.isShowSubtitle" @click="requestShowSubtitle(card)">&dArr;</div>
+        <div class="isFavorite" title="Add to Favorite" v-if="!card.isFavorite" @click="requestAddToFavorite(card)">&#9829;</div>
+        <div class="isFavorite isFavorite--true" title="Delete From Favorite" v-if="card.isFavorite" @click="requestDeleteFromFavorite(card)">&#9829;</div>
       </div>
     </section>
   `,
@@ -18,10 +20,26 @@ const cardComponent = {
       default() {
         return {id: 100};
       }
+    },
+  },
+  methods: {
+    requestRemoveCard(card) {
+      this.$emit('card-removed', card);
+    },
+    requestHideSubtitle(card) {
+      this.$emit('cardsubtitle-hidden', card);
+    },
+    requestShowSubtitle(card) {
+      this.$emit('cardsubtitle-shown', card);
+    },
+    requestAddToFavorite(card) {
+      this.$emit('added-to-favorite', card);
+    },
+    requestDeleteFromFavorite(card) {
+      this.$emit('deleted-from-favorite', card);
     }
-  }
+  },
 }
-
 
 Vue.component('cards', {
   props: {
@@ -31,8 +49,8 @@ Vue.component('cards', {
     'card': cardComponent,
   },
   methods: {
-    requestRemoveCard(index) {
-      this.$emit('card-removed', index);
+    requestRemoveCard(card) {
+      this.$emit('card-removed', card);
     },
     requestHideSubtitle(card) {
       this.$emit('cardsubtitle-hidden', card);
@@ -40,11 +58,23 @@ Vue.component('cards', {
     requestShowSubtitle(card) {
       this.$emit('cardsubtitle-shown', card);
     },
+    requestAddToFavorite(card) {
+      this.$emit('added-to-favorite', card);
+    },
+    requestDeleteFromFavorite(card) {
+      this.$emit('deleted-from-favorite', card);
+    }
   },
   template: `
     <div class="row">
-      <card v-for="(card, index) in cards" :key="card.id" :card="card">
-
+      <card v-for="(card, index) in cards" 
+        :key="card.id" 
+        :card="card" 
+        @card-removed="requestRemoveCard(card)" 
+        @added-to-favorite="requestAddToFavorite(card)" 
+        @deleted-from-favorite="requestDeleteFromFavorite" 
+        @cardsubtitle-shown="requestShowSubtitle(card)" 
+        @cardsubtitle-hidden="requestHideSubtitle(card)">
       </card>
     </div>
   `,
@@ -54,8 +84,6 @@ Vue.component('cards', {
   }
 })
 
-
-
 new Vue({
   el: '#app',
   data: {
@@ -64,21 +92,25 @@ new Vue({
         id: 1,
         title: 'Seitan polaroid flannel',
         subtitle: 'next level',
-        isShowSubtitle: false
+        isShowSubtitle: false,
+        isFavorite: false,
       },
       { id: 2, 
-        title: 'Street art swag' 
+        title: 'Street art swag',
+        isFavorite: false,
       },
       {
         id: 3,
         title: 'Deep v selvage tousled',
         subtitle: 'tousled copper mug, gochujang crucifix try-hard tbh',
-        isShowSubtitle: false
+        isShowSubtitle: false,
+        isFavorite: false,
       },
     ],
     title: '',
     subtitle: '',
     count: 3,
+    inFavorites: [],
   },
   
   methods: {
@@ -88,8 +120,9 @@ new Vue({
     removeLast() {
       this.cards.pop();
     },
-    removeCard(index) {
-      this.cards.splice(index, 1);
+    removeCard(card) {
+      this.cards.splice(this.cards.indexOf(card), 1);
+      this.deleteFromFavorite(card);
     },
     hideSubtitle(card) {
       card.isShowSubtitle = false;
@@ -103,10 +136,25 @@ new Vue({
         id: this.count,
         title: this.title,
         subtitle: this.subtitle,
-        isShowSubtitle: false
+        isShowSubtitle: false,
+        isFavorite: false
       });
       this.title = '';
       this.subtitle = '';
+    },
+    addToFavorite(card) {
+      if (this.inFavorites.includes(card.id) === true) {
+        return;
+      }
+      this.inFavorites.push(card.id);
+      card.isFavorite = true;
+    },
+    deleteFromFavorite(card) {
+      if (this.inFavorites.includes(card.id) === false) {
+        return;
+      }
+      this.inFavorites.splice(this.inFavorites.indexOf(card.id), 1);
+      card.isFavorite = false;
     }
   },
 });
